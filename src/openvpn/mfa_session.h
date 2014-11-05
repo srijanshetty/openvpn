2a668d815ad2190a6b24cee65b6bd5b67160ebe1
Harshvardhan Sharma <harsh1618@outlook.com>
2014-11-05 16:52:50 +0530

    create new cookie in key_method_2_write

--- src/openvpn/mfa_session.h
+++ src/openvpn/mfa_session.h
@@ -30,6 +30,8 @@
 #define MFA_SESSION_H
 
 #include "socket.h"
+#include "ssl_common.h"
+#include "ssl_verify.h"
 
 #ifdef ENABLE_MFA
 #define MAX_MFA_SESSIONS 128
@@ -37,12 +39,14 @@
 #define MFA_COOKIE_KEY_LENGTH 32  /* in bytes */
 #define MFA_COOKIE_HASH_LENGTH 32
 #define MFA_TIMESTAMP_LENGTH 20
+#define REMOTE_ADDRESS_LENGTH 64
+
 struct mfa_session_info
 {
-  char *cn;
-  char *token;
-  unsigned char *remote_address;
-  char *timestamp;
+  char common_name[TLS_USERNAME_LEN];
+  char token[MFA_TOKEN_LENGTH];
+  char remote_address[REMOTE_ADDRESS_LENGTH];
+  char timestamp[MFA_TIMESTAMP_LENGTH];
 };
 
 struct mfa_session_store
@@ -54,11 +58,11 @@ struct mfa_session_store
 struct mfa_session_info *
 get_cookie (const struct openvpn_sockaddr *dest, struct mfa_session_store *store);
 
-struct mfa_session_info *
-create_cookie ();
+void
+create_cookie (struct tls_session *session, struct mfa_session_info *cookie);
 
 void
-verify_cookie (tls_session *session, mfa_session_info *cookie);
+verify_cookie (struct tls_session *session, struct mfa_session_info *cookie);
 #endif
 
 #endif
